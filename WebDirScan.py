9c19bc48016e0c452566e95d5a5c425ed9fd8562
T1mePass <2636267635@qq.com>
2024-04-23 19:03:26 +0800

    proxy
    1. 自定义代理地址
    2. 增加是否使用代理池选项

--- WebDirScan.py
+++ WebDirScan.py
@@ -5,13 +5,15 @@ import sys
 import threading
 from agent_proxy import user_agent_list
 from agent_proxy import ip_proxy
+import re
 
 class DirScan(threading.Thread):
 
-    def __init__(self,queue):
+    def __init__(self,queue,isRandomProxy,proxy):
         threading.Thread.__init__(self)
         self._queue = queue
-    
+        self.isRandomProxy = isRandomProxy
+        self.proxy = proxy
 
     def run(self):
 
@@ -21,7 +23,12 @@ class DirScan(threading.Thread):
             # print(user_agent_list.get_user_agent())
 
             try:
-                req = requests.get(url=url,headers= user_agent_list.get_user_agent(),timeout=8,proxies=ip_proxy.get_ip_proxy())
+                if self.isRandomProxy and self.proxy==0 :
+                    req = requests.get(url=url,headers= user_agent_list.get_user_agent(),timeout=8,proxies=ip_proxy.get_ip_proxy())
+                elif self.proxy :
+                    req = requests.get(url=url,headers= user_agent_list.get_user_agent(),timeout=8,proxies=self.proxy)
+                else:
+                    req = requests.get(url=url, headers=user_agent_list.get_user_agent(), timeout=8)
                 # print(url)
 
                 if req.status_code == 200:
@@ -32,7 +39,7 @@ class DirScan(threading.Thread):
     
 
 # ext 字典名称
-def start(url, ext, count):
+def start(url, ext, count,isRandomProxy,proxy):
     queue = Queue()
 
 
@@ -46,7 +53,7 @@ def start(url, ext, count):
     thread_count = int(count)
 
     for i in range(thread_count):
-        threads.append(DirScan(queue))
+        threads.append(DirScan(queue,isRandomProxy,proxy))
 
     for t in threads:
         t.start()
@@ -54,7 +61,6 @@ def start(url, ext, count):
         t.join()
 
     f.close()
-    
 
 # if __name__ == '__main__':
 #     if len(sys.argv) !=4:
@@ -63,6 +69,23 @@ def start(url, ext, count):
 #     else:
 #         # print(sys.argv[1], sys.argv[2], sys.argv[3])
 #         start(sys.argv[1],sys.argv[2],sys.argv[3])
+
+    
+#format : http://192.168.1.1:8080
+def proxy_handle(proxy):
+    pattern = r'(http?|https)://(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})'
+    match = re.match(pattern,proxy)
+
+    if match:
+        protocol = match.group(1)
+        ip = match.group(2)
+        port = match.group(3)
+
+        return f'{{"{protocol}": "{protocol}://{ip}:{port}"}}'
+
+    else:
+        return "Invalid proxy format."
+
 
 
 
@@ -81,12 +104,21 @@ __        __   _     ____  _      ____
     parser.add_argument("-u", "--url", dest="url", help="set target url", required=True)
     parser.add_argument("-f", "--file", dest="ext", help="target url ext", required=True)
     parser.add_argument("-t", "--thread", dest="count", type=int, default=2, help="set scan thread count")
-
+    parser.add_argument("-random_proxy",dest="isRandomProxy",type=bool,default=0,help="use random proxy, use for True, not for False",required=False)
+    parser.add_argument("-proxy",dest="proxy",default=0,help="set your own proxy, format is http://192.168.1.1:8080",required=False)
 
     args = parser.parse_args()
 
     if args.url and args.ext:
-        start(args.url,args.ext,args.count)
+        proxy = args.proxy
+        handle_proxy = 0
+        if proxy != 0:
+            handle_proxy = proxy_handle(proxy)
+            if handle_proxy == "Invalid proxy format.":
+                print("Invalid proxy format.")
+                exit(0)
+
+        start(args.url,args.ext,args.count,args.isRandomProxy,handle_proxy)
         sys.exit(1)
     else:
         parser.print_help()
